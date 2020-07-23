6b630b28a44af23aa07d7b58a1b001a3cd45244c
jessicachen <C19jessica.chen@usafa.edu>
2020-07-22 21:34:29 -0400

    completed lesson

---
+++ src/main/java/com/example/springcrudl/LessonRepository.java
@@ -0,0 +1,8 @@
+package com.example.springcrudl;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+
+}
---
+++ src/main/java/com/example/springcrudl/SpringCrudlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudlApplication.class, args);
+	}
+
+}
